8ae9d201e245588704b17b31b154fa39e26129f5
Yash Arora <yash.arora1@MacBook-Pro.bbrouter>
2022-08-08 17:00:49 +0530

    Added comsumer

---
+++ consumer1/consumer1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		GroupID: "consumer-group-1",
+		Topic:   "example-123",
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+	}
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+}
